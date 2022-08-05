538d9681b2546fb0bb9eb991265958e06cd6a4a9
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2022-08-04 20:50:51 -0400

    add landing page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, LandingPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('landing/', LandingPageView.as_view(), name='landing'),
 ]
+
