e5307383021093fbe2b00c523f59ea40cafd0cde
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2022-08-08 19:19:57 -0400

    add by book view

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from .views import upload_file
-
 from .views import (
     HomePageView,
     LandingPageView,
@@ -8,6 +6,9 @@ from .views import (
     SmartFeedView,
     ByBookView,
     ByTagView,
+    logout_user,
+    book_inside_view,
+    upload_file,
 )
 
 urlpatterns = [
@@ -16,5 +17,7 @@ urlpatterns = [
     path("upload/", upload_file, name="upload"),
     path("smart_feed/", SmartFeedView.as_view(), name="smart_feed"),
     path("by_book/", ByBookView.as_view(), name="by_book"),
+    path("by_book/<int:id>", book_inside_view, name="book_page"),
     path("by_tag/", ByTagView.as_view(), name="by_tag"),
+    path("logout/", logout_user, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from gc import get_objects
+from multiprocessing import context
+from django.views.generic import TemplateView, DetailView
 from django.views.generic.list import ListView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
@@ -6,10 +8,13 @@ from django.urls import reverse_lazy
 from .kindle_parser import start_kindle_parser
 from .forms import FileForm
 from django.core.files.storage import FileSystemStorage
-from .models import UserFiles, Quote
+from .models import UserFiles, Quote, Book
 import random
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 
 class HomePageView(TemplateView):
@@ -37,12 +42,75 @@ class SmartFeedView(LoginRequiredMixin, TemplateView):
     template_name = "smart_feed.html"
 
 
-class ByBookView(LoginRequiredMixin, TemplateView):
+# class ByBookView(LoginRequiredMixin, TemplateView):
+#     template_name = "by_book.html"
+
+
+# @login_required
+# def by_book(request):
+#     books_list = Book.query.filter(Book.owner == request.user.id).all()
+#     quotes_list = Quote.query.filter(Quote.owner == request.user.id).all()
+
+#     # Count how many highlights in each book
+#     highlights = {}
+#     for record in quotes_list:
+#         book_title = record.book_title_db
+#         quote = record.quote_db
+#         if book_title not in highlights:
+#             highlights[book_title] = [quote]
+#         else:
+#             highlights[book_title].append(quote)
+
+#     highlights_count = {}
+#     for key in highlights:
+#         highlights_count[key] = len(highlights[key])
+#     print(highlights_count)
+
+#     context = {
+#         "book_titles_and_highlights": books_list,
+#         "highlights_count": highlights_count,
+#     }
+
+#     return render(request, "by_book.html", context)
+
+
+class ByBookView(LoginRequiredMixin, ListView):
     template_name = "by_book.html"
+    model = Book
+    context_object_name = "books_list"
+
+    def get_queryset(self):
+        books_list = list(Book.objects.filter(owner=self.request.user.id))
+        quotes_list = list(Quote.objects.filter(owner=self.request.user.id))
+
+        # Count how many highlights in each book
+        highlights = {}
+        for record in quotes_list:
+            book_title = record.book_title_db
+            quote = record.quote_db
+            if book_title not in highlights:
+                highlights[book_title] = [quote]
+            else:
+                highlights[book_title].append(quote)
+
+        highlights_count = {}
+        for key in highlights:
+            highlights_count[key] = len(highlights[key])
+        # print(highlights_count)
+
+        return books_list
 
 
 class ByTagView(LoginRequiredMixin, TemplateView):
     template_name = "by_tag.html"
+
+
+@login_required
+def book_inside_view(request, id):
+    list_of_quotes = get_list_or_404(Quote, book_title_db=id)
+    book_title = get_object_or_404(Book, id=id)
+    context = {"quotes_by_book": list_of_quotes, "book_title": book_title.book_title_db}
+    return render(request, "book_page.html", context)
 
 
 # FILE UPLOAD
@@ -67,3 +135,8 @@ def upload_file(request):
     else:
         form = FileForm()
     return render(request, "upload.html", {"form": form})
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("landing"))
