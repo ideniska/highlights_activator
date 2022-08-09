b6090b426dba331d46d8fada8d9c9ab91a4635da
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2022-08-09 17:30:05 -0400

    add show more toggle button to by book page

---
+++ static/index.js
@@ -0,0 +1,8 @@
+var content = document.querySelector(".dashboard-books")
+var button = document.getElementById("show-more")
+
+
+button.onclick = function() {
+    content.classList.toggle('dashboard-books');
+    console.log('click');
+}
