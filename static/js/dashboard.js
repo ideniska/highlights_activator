0c051c9b35d48ac2ed6d94b5ed36629161fef95c
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2022-08-30 19:20:27 -0400

    add edit quote and add comment css features to dashboard  without save to db

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -23,8 +23,12 @@ function randomQuote () {
             console.log(current_quote);
             $(".like-button").html('<div id="like">'+showCurrentLike(like, current_quote)+'</div>');
             $(".dashboard-delete").html('<div id="dash-delete" data-quoteId="'+current_quote+'"><i class="fa-solid fa-ban"></i></div>');
+            $(".dashboard-edit").html('<div id="dash-edit" data-quoteId="'+current_quote+'"><i class="fa-solid fa-pen-to-square"></i></div>');
             $(".dashboard-share").html(
-              '<div class="dropdown"><div data-bs-toggle="dropdown" aria-expanded="false" id="dash-share" data-quoteId="'+current_quote+'"><i class="fa-solid fa-share-nodes"></i></div><ul class="dropdown-menu"><li><a class="dropdown-item" href="#">Twitter</a></li><li><a class="dropdown-item" href="#">Facebook</a></li><li><a class="dropdown-item" href="#">Copy</a></li></ul></div>'
+              '<div class="dropdown"><div data-bs-toggle="dropdown" aria-expanded="false" id="dash-share"\
+               data-quoteId="'+current_quote+'"><i class="fa-solid fa-share-nodes"></i></div><ul class="dropdown-menu">\
+               <li><a class="dropdown-item" href="#">Twitter</a></li><li><a class="dropdown-item" href="#">Facebook</a></li>\
+               <li><a class="dropdown-item" href="#">Copy</a></li></ul></div>'
               );
         }
     });
@@ -110,6 +114,49 @@ $(document).on('click', '#dash-share', function(){
 });
 
 
-function shareQuote(quoteId) {
-  
-}
+function shareQuote(quoteId) { 
+};
+
+// EDIT AND COMMENT
+$(document).on('click', '#dash-edit', function(){
+  editQuote($(this).data("quoteid"));
+  addComment($(this).data("quoteid"));
+});
+
+// EDIT QUOTE, SHOW COMMENT BOX, SHOW CANCEL & SAVE BUTTONS
+function editQuote(quoteid) {
+  $("#quote-text").attr('contenteditable', 'true');
+  $(".card-text").html('<h5>Edit:</h5>');
+  $("#quote-text").css({
+    "background" : "#FEFAE0",
+  });
+  $(".comment").html('Add note:<div class="comment-box"></div>');
+  $(".comment-box").css({
+    "visibility" : "visible",
+    "height": "100px",
+  });
+  $(".comment-box").attr('contenteditable', 'true');
+  $(".comment-box-buttons").css({
+    "visibility" : "visible",
+    "height": "30px",
+  });
+};
+
+// CANCEL EDIT
+$(document).on('click', '#cancel', function(){
+  $("#quote-text").attr('contenteditable', 'false');
+  $(".card-text").html('');
+  $("#quote-text").css({
+    "background" : "white",
+  });
+  $(".comment").html('');
+  $(".comment-box").css({
+    "visibility" : "hidden",
+    "height": "0",
+  });
+  $(".comment-box").attr('contenteditable', 'false');
+  $(".comment-box-buttons").css({
+    "visibility" : "hidden",
+    "height": "0",
+  });
+});
