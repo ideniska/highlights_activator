d627f75701af673b98a236ded7e3474087887149
Denis Sakhno <97180503+ideniska@users.noreply.github.com>
2022-11-01 11:41:53 -0400

    prod cm2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python manage.py migrate --noinput
 #gunicorn project.wsgi:application --bind 0.0.0.0:8000 --reload
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
-python manage.py telegram_bot
